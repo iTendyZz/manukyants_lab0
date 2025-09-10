66058668cc6b2933ca1181ea02a3f4ac14e0eb80
iTendyZz <mbabayew78@gmail.com>
2025-09-11 00:35:50 +0300

    commit

---
+++ manukyantshelloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "Hello World!" << endl;
+	string mychar;
+	cout << "simvol: ";
+	cin >> mychar;
+	cout << "smotri: " << mychar << endl;
+	return 1;
+}
